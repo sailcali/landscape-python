59f32ff3205a625457c6706e4859e1d1846a1d18
Parsec <sailcali@gmail.com>
2021-12-12 21:12:43 -0800

    cleaned up, removed prints. Ready for CRON

--- main.py
+++ main.py
@@ -13,29 +13,20 @@ GPIO.setmode(GPIO.BCM)
 GPIO_PIN = 17
 GPIO.setup(GPIO_PIN, GPIO.OUT)
 
-for i in range(14):
-    city = lookup("San Diego", database())
-    s = sun(city.observer, date=date.today())
-    sunrise = s["sunrise"]
-    sunrise = sunrise.astimezone(tz=pytz.timezone("US/Pacific"))
-    sunset = s["sunset"]
-    sunset = sunset.astimezone(tz=pytz.timezone("US/Pacific"))
+city = lookup("San Diego", database())
+s = sun(city.observer, date=date.today())
+sunrise = s["sunrise"]
+sunrise = sunrise.astimezone(tz=pytz.timezone("US/Pacific"))
+sunset = s["sunset"]
+sunset = sunset.astimezone(tz=pytz.timezone("US/Pacific"))
 
-    print(sunrise.time())
-    print(sunset.time())
-
-    if sunset.time() < datetime.now().time() > sunrise.time():
-        print('on')
-        if GPIO.input(GPIO_PIN):
-            print('is on')
-        else:
-            print('turning on')
-            GPIO.output(GPIO_PIN, GPIO.HIGH)
+if sunset.time() < datetime.now().time() > sunrise.time():
+    if GPIO.input(GPIO_PIN):
+        pass
     else:
-        if not GPIO.input(GPIO_PIN):
-            print('is off')
-        else:
-            print('turning off')
-            GPIO.output(GPIO_PIN, GPIO.LOW)
-    time.sleep(60)
-GPIO.cleanup()
+        GPIO.output(GPIO_PIN, GPIO.HIGH)
+else:
+    if not GPIO.input(GPIO_PIN):
+        pass
+    else:
+        GPIO.output(GPIO_PIN, GPIO.LOW)
