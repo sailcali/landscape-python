f4e08e7d38b98ecaf3d5cf517381c26946ac8651
Parsec <sailcali@gmail.com>
2021-12-27 22:00:30 -0800

    changes to state update the server

--- main.py
+++ main.py
@@ -5,26 +5,10 @@ from astral.sun import sun
 from datetime import date
 import pytz
 from astral.geocoder import database, lookup
-from datetime import datetime, timedelta
-# from sqlalchemy import create_engine
-# from dotenv import load_dotenv, set_key, find_dotenv
-# import os
-# import pandas as pd
+from datetime import datetime
+import requests
 
-# Set globals
-# DOTENV_FILE = find_dotenv()
-# load_dotenv(DOTENV_FILE)
-# DB_STRING = os.environ.get('DB_STRING')
-# data = {'time': [datetime.today(),], 'device': 'landscape'}
-
-# Open database engine
-# db = create_engine(DB_STRING)
-
-# Get last time entry from database
-# with db.connect() as con:
-#     sql = """SELECT time FROM lighting_status WHERE device = 'landscape' ORDER BY time DESC LIMIT 1;"""
-#     result = con.execute(sql)
-#     last_time = result.fetchone()[0]
+data = {'time': datetime.today(), 'device': 'landscape'}
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -44,19 +28,13 @@ if sunset.time() < datetime.now().time():
         quit()
     else:
         GPIO.output(GPIO_PIN, GPIO.HIGH)
-        # data['setting'] = True
+        data['setting'] = True
 else:
     if not GPIO.input(GPIO_PIN):
         quit()
     else:
         GPIO.output(GPIO_PIN, GPIO.LOW)
-        # data['setting'] = False
-        # time_on = datetime.now() - last_time
-        # data['time_on'] = time_on.seconds//3600
+        data['setting'] = False
         
-# # Create dataframe from sensor data
-# df = pd.DataFrame(data)
-# df.set_index(['time'], inplace=True)
-
 # # Send new data to database
-# df.to_sql('lighting_status', db, if_exists='append')
+requests.post('http://192.168.86.31/landscape/update-state', params=data)
