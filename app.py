bddccb429aa40b96ec6cd648266ca52160bafb75
Parsec <sailcali@gmail.com>
2021-12-28 14:44:43 -0800

    added delay time to status

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import RPi.GPIO as GPIO
 from get_temps import get_pi_details
 from main import change_landscape
 from flask_cors import CORS
+import configparser
+
+config = configparser.ConfigParser()
 
 app = Flask(__name__)
 CORS(app)
@@ -18,6 +21,8 @@ def change_landscape_state():
 
 @app.route('/get-status', methods=['GET'])
 def get_status():
+    config.read_file(open(r'delay_time.conf'))
+    delay_value = config.get('DelayDatetime', 'value')
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
     GPIO_PIN = 27
@@ -25,7 +30,7 @@ def get_status():
     temperature, humidity = get_pi_details()
     lighting_state = GPIO.input(GPIO_PIN)
     response = {'temperature': temperature, 'humidity': humidity, 
-                'lighting_state': lighting_state}
+                'lighting_state': lighting_state, 'current_delay': delay_value}
     return jsonify(response), 200
 
 
