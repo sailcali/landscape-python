5ccca807792572dbffb993a98c0a4cf2b7e54a91
Parsec <sailcali@gmail.com>
2022-02-23 20:40:19 -0800

    Added led function to pin 17 for temps

--- get_temps.py
+++ get_temps.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
+
 import Adafruit_DHT as dht
 import time
+import RPi.GPIO as GPIO
+import requests
 
 DHT_TRIES = 6
 
@@ -16,3 +20,21 @@ def get_pi_details():
         return farenheight, humidity
     except Exception:
         time.sleep(2)
+
+def update_led(temp):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO_PIN = 17
+    GPIO.setup(GPIO_PIN, GPIO.OUT)
+    response = requests.get('http://192.168.86.205/api/currrent_temps')
+    current_temps = response.json()
+    if current_temps['living_room_temp'] <= temp:
+        if GPIO.input(GPIO_PIN):
+            GPIO.output(GPIO_PIN, GPIO.LOW)
+    else:
+        if not GPIO.input(GPIO_PIN):
+            GPIO.output(GPIO_PIN, GPIO.HIGH)
+
+if __name__ == "__main__":
+    temp, humidity = get_pi_details()
+    update_led(temp)
